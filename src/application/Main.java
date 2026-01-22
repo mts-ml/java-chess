fd6639a715f53cbba32e2676946b917a93cf6bed
Mateus Lima <mtsmlima@gmail.com>
2026-01-22 12:48:04 -0300

    test(Position): Add data to test class Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, word!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
