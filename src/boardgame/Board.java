b40ce29346caa829bcc274a98df1fbcecd3d0002
Mateus Lima <mtsmlima@gmail.com>
2026-01-22 13:01:20 -0300

    feat(class): Implement classes Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Board {
+
+    private Integer rows;
+    private Integer columns;
+    private Piece[][] pieces;
+
+
+    public Board() {
+    }
+
+    public Board(Integer rows, Integer columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.pieces = new Piece[rows][columns];
+    }
+
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getColumn() {
+        return columns;
+    }
+
+    public void setColumn(Integer column) {
+        this.columns = column;
+    }
+}
