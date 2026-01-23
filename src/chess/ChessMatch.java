8d17f7a1a35c12a67ec7df7031ab71be92a648ea
Mateus Lima <mtsmlima@gmail.com>
2026-01-23 14:58:43 -0300

    fix(chess): Fix initialSetup()

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,9 +32,6 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
@@ -47,7 +44,6 @@ public class ChessMatch {
         placeNewPiece('d', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new King(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
